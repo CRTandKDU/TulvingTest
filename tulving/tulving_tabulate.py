f619bc7cd68d8a0d22056d40d0de4acd5a1d98e7
CRTandKDU <jeanmariechauvet@gmail.com>
2024-02-19 19:39:18 +0100

    Updated text

--- tulving/tulving_tabulate.py
+++ tulving/tulving_tabulate.py
@@ -1,9 +1,11 @@
 # tulving_tabulate.py -- Tabulating results of the "direct comparison"
-# Revised: Wednesday, February 14, 2024
+# Revised: Wednesday, February 14, 2024 -- Added ordering tests results
+# Revised: Sunday, February 18, 2024 -- The question of cue valence
 
 import argparse
 import pickle
 import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
 
 CUETYPES        = ["'copy'", "'ncaw'", "'ncrw'", "'none'"]
@@ -63,7 +65,7 @@ def tulving_results( chrono, false_positives=True ):
 
 
 def tulving_occurences( tbr, nsessions, dfs, cuetype ):
-    occurrences = np.zeros( len(tbr) )
+    occ_word, occ_true = np.zeros( len(tbr) ), np.zeros( len(tbr) )
     iw = 0
     for w in tbr:
         for i in range( nsessions ):
@@ -71,10 +73,12 @@ def tulving_occurences( tbr, nsessions, dfs, cuetype ):
             if df.empty :
                 pass
             else:
-                if( 1 == df.iat[ 0, 1 ] and df.iat[ 0, 2 ] ) in cuetype :
-                    occurrences[iw] += 1
+                if df.iat[ 0, 2 ]  in cuetype:
+                    occ_word[iw] += 1
+                if 1 == df.iat[ 0, 1 ] and df.iat[ 0, 2 ] in cuetype :
+                    occ_true[iw] += 1
         iw += 1
-    return occurrences
+    return occ_word, occ_true
 
 
 def tulving_cuetype_results( chrono, cuetype, false_positives=True ):
@@ -96,10 +100,10 @@ def tulving_cuetype_results( chrono, cuetype, false_positives=True ):
     # Score each word
     with open( TESTWORDS, 'rb' ) as f:
         tbr = pickle.load( f )
-    occurrences_o = tulving_occurences( tbr, len(SESSIONS), dfo, cuetype )
-    occurrences_a = tulving_occurences( tbr, len(SESSIONS), dfa, cuetype )
-    return( pd.DataFrame( data={ 'words': tbr, 'occurrences': occurrences_o } ),
-            pd.DataFrame( data={ 'words': tbr, 'occurrences': occurrences_a } ) )
+    occ_words_o, occ_true_o = tulving_occurences( tbr, len(SESSIONS), dfo, cuetype )
+    occ_words_a, occ_true_a = tulving_occurences( tbr, len(SESSIONS), dfa, cuetype )
+    return( pd.DataFrame( index=tbr, data={ 'occ_word': occ_words_o, 'occ_true': occ_true_o } ),
+            pd.DataFrame( index=tbr, data={ 'occ_word': occ_words_a, 'occ_true': occ_true_a} ) )
         
 
 def summary( df, fp ):
@@ -116,6 +120,28 @@ def summary( df, fp ):
     summary = df_clean.groupby( ["'cue_type'"] ).mean()[COLRES]
     return summary
     
+
+def rand_jitter(arr):
+    stdev = .01 * (max(arr) - min(arr))
+    return arr + np.random.randn(len(arr)) * stdev
+
+
+def tulving_scatterplot( df, title ):
+    # dfo_sorted = df.sort_values( by=['reco_true'], ascending=False )
+    # df[['B','C']] = df[['B','C']].div(df.A, axis=0)
+
+    df[ ['reco_true'] ] = df[ ['reco_true'] ].div( df.reco_word, axis = 0 )
+    df[ ['reca_true'] ] = df[ ['reca_true'] ].div( df.reca_word, axis = 0 )
+
+    # df.reco_true = rand_jitter( df.reco_true )
+    # df.reca_true = rand_jitter( df.reca_true )
+    xyrange = [ -0.05, 1.05 ]
+    df.plot.scatter( x='reca_true', y='reco_true', 
+                     title=title, marker='+',
+                     xlim=xyrange, ylim=xyrange,
+                     xlabel='Cue Valence', ylabel='Recognition'
+                    )
+
 
 def main():
     global SESSIONS
@@ -151,7 +177,7 @@ def main():
         treco, treca               = reco.count().get("'cue_type'"), reca.count().get("'cue_type'")
         print( f'* {chrono} Task\n:PROPERTIES:\n:SessionIds: {SESSIONS}\n:Recos: {treco:3d}\n:Recas: {treca:3d}\n:END:' )
 
-        # Pretty print statistical summaries
+        # Org-mode print statistical summaries
         if not args.book :
             df = pd.merge( summary_reco, summary_reca, on="'cue_type'" ).rename(
                 columns={"'result'_x": "Familiarity", "'result'_y": "Identification"},
@@ -164,17 +190,27 @@ def main():
         df.index.rename( "Cue Type", inplace=True )
         print( df )
 
-        # Pretty print most recognized/recalled words
+        # Org-mode print most recognized/recalled words
         print()
         print( f'* {chrono} Task: Most recognized and recalled' )
         print( '** Copy Cues' )
         dfo, dfa = tulving_cuetype_results( chrono, [ "'copy'" ], false_positives = not args.book )
-        df = pd.concat( [dfo.rename( columns={ 'occurrences': 'reco' } ), dfa['occurrences'].rename( 'reca' )], axis=1 )
-        print( df.sort_values( by=['reco'] ) )
+        df = pd.concat( [dfo.rename( columns={ 'occ_word': 'reco_word', 'occ_true': 'reco_true' } ), dfa.rename( columns={ 'occ_word': 'reca_word', 'occ_true': 'reca_true' } ) ], axis=1 )
+        df.to_csv( f'tab_counts_copy_{chrono}.csv' )
+        # print( df.sort_values( by=['reco'] ) )
+        # Scatterplot
+        tulving_scatterplot( df, f'{chrono} Task -- Copy Cues' )
+        plt.show()
+
+        plt.close( "all" )
         print( '** Associative Cues' )
-        dfo, dfa = tulving_cuetype_results( chrono, [ "'ncaw'" ], false_positives = not args.book )
-        df = pd.concat( [dfo.rename( columns={ 'occurrences': 'reco' } ), dfa['occurrences'].rename( 'reca' )], axis=1 )
-        print( df.sort_values( by=['reco'] ) )
+        dfo, dfa = tulving_cuetype_results( chrono, [ "'ncaw'", "'ncrw'" ], false_positives = not args.book )
+        df = pd.concat( [dfo.rename( columns={ 'occ_word': 'reco_word', 'occ_true': 'reco_true' } ), dfa.rename( columns={ 'occ_word': 'reca_word', 'occ_true': 'reca_true' } ) ], axis=1 )
+        df.to_csv( f'tab_counts_noncopy_{chrono}.csv' )
+        tulving_scatterplot( df, f'{chrono} Task - Associative Cues' )
+        plt.show()
+        # df.sort_values( by=['reco_true'], ascending=False ).to_csv( f'tab_counts_copy_{chrono}.csv' )
+        # print( df.sort_values( by=['reco'] ) )
 
                          
 if __name__ == '__main__':
